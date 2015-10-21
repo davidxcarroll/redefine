7df753413b54e9a1d9958fd56cb8a0a05291cc71
David <kansas@inkwhiskey.com>
2015-10-21 17:03:24 -0400

    hooked up logo and menus

--- redefine/gulpfile.js
+++ redefine/gulpfile.js
@@ -8,7 +8,8 @@ var sass = require('gulp-sass');
 gulp.task('sass', function() {
     gulp.src('_src/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./_public/_css'));
+        .pipe(gulp.dest('./_public/static/_css'))
+        .pipe(gulp.dest('./_public/wp-local/wp-content/themes/redefine-theme/_css'));
 });
 
 // WATCH
