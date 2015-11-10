ec59b555784bb357969f22fdfd2be4d3d66247ee
David <kansas@inkwhiskey.com>
2015-11-10 18:28:08 -0500

    half way through responsive

--- redefine/_public/static/_js/main.js
+++ redefine/_public/static/_js/main.js
@@ -40,6 +40,7 @@ $(document).ready(function(){
     $("#navigation-home").toggleClass("mobile-nav-expanded");
     $("#icon-nav-mobile").toggleClass("icon-nav-hide");
     $("#icon-nav-mobile-close").toggleClass("icon-nav-hide");
+    console.log("hey");
   });
 });
 
@@ -90,3 +91,44 @@ $("#faq-careers-button").click(function() {
   $("#faq-careers-button").addClass("selected");
 });
 
+// MOBILE MENU DISABLE SCROLL
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+// var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+// if ($("#navigation").hasClass("mobile-nav-expanded")) {
+
+//   function preventDefault(e) {
+//     e = e || window.event;
+//     if (e.preventDefault)
+//       e.preventDefault();
+//     e.returnValue = false;  
+//   }
+
+//   function preventDefaultForScrollKeys(e) {
+//     if (keys[e.keyCode]) {
+//       preventDefault(e);
+//       return false;
+//     }
+//   }
+
+//   function disableScroll() {
+//     if (window.addEventListener) // older FF
+//       window.addEventListener('DOMMouseScroll', preventDefault, false);
+//     window.onwheel = preventDefault; // modern standard
+//     window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+//     window.ontouchmove  = preventDefault; // mobile
+//     document.onkeydown  = preventDefaultForScrollKeys;
+//   }
+
+//   function enableScroll() {
+//     if (window.removeEventListener)
+//       window.removeEventListener('DOMMouseScroll', preventDefault, false);
+//     window.onmousewheel = document.onmousewheel = null; 
+//     window.onwheel = null; 
+//     window.ontouchmove = null;  
+//     document.onkeydown = null;  
+//   }
+
+// }
